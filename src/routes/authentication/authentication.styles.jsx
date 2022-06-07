301f4628147d640926c2474b1e99b33829556b90
Lenin <leninmanueld@gmail.com>
2022-06-07 10:15:44 -0400

    modified sign in page to fit mobile browser

--- src/routes/authentication/authentication.styles.jsx
+++ src/routes/authentication/authentication.styles.jsx
@@ -3,4 +3,10 @@ import styled from 'styled-components'
 export const SignContainer = styled.div`
     display: flex;
     justify-content: space-around;
+    @media only screen 
+    and (min-device-width : 200px) 
+    and (max-device-width : 480px) {
+        width: 90%;
+  }
+
 `;
