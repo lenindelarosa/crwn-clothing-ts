c996f19c4123b3e179d0e2e59da3d4c77765b911
Lenin <leninmanueld@gmail.com>
2022-06-07 10:45:31 -0400

    modified authentication page for mobile UI

--- src/components/sign-in-form/sign-in-form.styles.jsx
+++ src/components/sign-in-form/sign-in-form.styles.jsx
@@ -3,14 +3,30 @@ import styled from 'styled-components';
 export const SignInContainer = styled.div`
     display: flex;
     flex-direction: column;
-    width: 45%;  
+    //width: 45%;  
+
+    @media only screen 
+    and (min-device-width : 200px) 
+    and (max-device-width : 480px) {
+      width: 90%;
+    }
 
     h2 {
         margin: 10px 0;   
+        font-size: min(max(20px, 2.5vw), 36px);
+    }
+
+    span {
+        font-size: min(max(12px, 2vw), 22px);
     }
 `;
 
 export const ButtonsContainer = styled.div`
   display: flex;
   justify-content: space-between;
+
+  button {
+    font-size: min(max(8px, 2vw), 16px);
+  }
+  
 `;
--- src/components/sign-up-form/sign-up-form.styles.jsx
+++ src/components/sign-up-form/sign-up-form.styles.jsx
@@ -3,9 +3,19 @@ import styled from 'styled-components';
 export const SignUpContainer = styled.div`
     display: flex;
     flex-direction: column;
-    width: 45%;
+    
+    @media only screen 
+    and (min-device-width : 200px) 
+    and (max-device-width : 480px) {
+      width: 90%;
+    }
 
     h2 {
         margin: 10px 0;
+        font-size: min(max(20px, 2.5vw), 36px);
+    }
+
+    span {
+        font-size: min(max(12px, 2vw), 22px);
     }
 `;
